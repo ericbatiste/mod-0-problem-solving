e5f543bce681c512a28328233228666e02968b0e
Eric Batiste <ericbatiste4@gmail.com>
2023-09-06 09:02:40 -0600

    Complete 2nd challenge

---
+++ challenge2.js
@@ -0,0 +1,9 @@
+// Define strings array with upper & lower case letters.
+// Itereate through array
+// Call .toLowerCase method on each item.
+// Print each modified array item.
+
+var words = ["wAcKy", "WiLd", "WoNdErFuL", "wIsDoM"];
+words.forEach((word) => console.log(word.toLowerCase()));
+
+// Prints: "wacky" "wild" "wonderful" "wisdom"
